fa8a5fb41767bab9896ded1c003168cc736789b7
wjosala67 <wladimir.salazar@gmail.com>
2020-05-30 16:44:54 -0500

    confirmado

---
+++ com.ws.cvxpress.iOS/CustomRenderers/CropViewRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using com.ws.cvxpress.Classes;
+using com.ws.cvxpress.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer( typeof(CropView) , typeof(CropViewRenderer))]
+
+namespace com.ws.cvxpress.iOS.CustomRenderers
+{
+    public class CropViewRenderer: PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            var page = Element as CropView;
+            if (page != null)
+            { 
+
+            }
+            }
+    }
+}
